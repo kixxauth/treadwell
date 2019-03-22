d061af5040a7b1a464b8bb5497cee442fe36451e
Kris Walker <kris@kixx.name>
2019-03-22 05:04:40 -0400

    Clean out index.js

--- index.js
+++ index.js
@@ -1,17 +1,2 @@
 'use strict';
 
-var TaskRunner = require('./lib/task-runner');
-
-var _currentTaskRunner;
-
-exports.TaskRunner = TaskRunner;
-
-exports.getCurrentTaskRunner = function getCurrentTaskRunner() {
-	if (_currentTaskRunner) {
-		return _currentTaskRunner;
-	}
-
-	_currentTaskRunner = TaskRunner.create();
-	return _currentTaskRunner;
-};
-
