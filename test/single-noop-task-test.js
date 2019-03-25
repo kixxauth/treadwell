84f52797881c95dfd6af3c48f687e222def27789
Kris Walker <kris@kixx.name>
2019-03-25 06:22:31 -0400

    Emitting task start and end events.
     Changes to be committed:
    	modified:   lib/task-runner.js
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   test/single-noop-task-test.js

--- test/single-noop-task-test.js
+++ test/single-noop-task-test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const { assert } = require('kixx-assert');
+const sinon = require('sinon');
 
 const { TaskRunner } = require('../index');
 
@@ -15,8 +16,14 @@ module.exports = function (test) {
 	test.describe('with default logger', (t) => {
 		let treadwell;
 
+		const taskStartListener = sinon.spy();
+		const taskEndListener = sinon.spy();
+
 		t.before((done) => {
 			treadwell = TaskRunner.create();
+
+			treadwell.on('task:start', taskStartListener);
+			treadwell.on('task:end', taskEndListener);
 
 			treadwell.task('foo:noop');
 
@@ -30,8 +37,12 @@ module.exports = function (test) {
 			process.nextTick(done);
 		});
 
-		t.it('is not smoking', () => {
-			assert.isOk(true, 'is not smoking');
+		t.it('fires task:start listener', () => {
+			assert.isEqual(1, taskStartListener.callCount);
+		});
+
+		t.it('fires task:end listener', () => {
+			assert.isEqual(1, taskEndListener.callCount);
 		});
 	});
 };
